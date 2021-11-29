502e42dc6cf3516e2bfd12707d71f00551b47c31
BMHebert <70716012+BMHebert@users.noreply.github.com>
2021-11-29 17:22:17 -0600

    Created CourseManagement and Course classes and added tests for createCourse method
    Created a skeleton class for Course management and Course with outlines of the methods that will go there and created tests for the createCourse method from the CourseManagement class.

---
+++ course_lab_management/CourseManagement.py
@@ -0,0 +1,27 @@
+# createCourse(courseName, courseTime, courseDays, courseHours, courseInstructor, courseTA)
+# editCourse(courseName, courseTime, courseDays, courseHours, courseInstructor, courseTa)
+# deleteCourse(courseName)
+# populateSearchClass(searchPromp)  should change class to course
+# displayAllCourse()
+
+class CourseManagement:
+
+    @staticmethod
+    def createCourse(courseName, courseTime, courseDays, courseHours, courseInstructor, courseTA):
+        pass
+
+    @staticmethod
+    def editCourse(courseName, courseTime, courseDays, courseHours, courseInstructor, courseTA):
+        pass
+
+    @staticmethod
+    def deleteCourse(courseName):
+        pass
+
+    @staticmethod
+    def populateSearchClass(searchPromp):
+        pass
+
+    @staticmethod
+    def displayAllCourse():
+        pass
