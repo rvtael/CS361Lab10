843e0449f94332513fabb85630fda996c3a267b6
YazanSalem <yazansalem064@gmail.com>
2021-12-03 10:57:31 -0600

    Added documentation to methods

--- TAScheduler/NonDjangoClasses/user_and_login/User.py
+++ TAScheduler/NonDjangoClasses/user_and_login/User.py
@@ -17,47 +17,104 @@
 
 class User:
 
+    
     def User(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+    # Postconditions: Sets the id of the user
+    # Side-effects: None
+    # User ID(in) - Id of the user
     def setId(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+    # Postconditions: Sets the name of the user
+    # Side-effects: None
+    # User Name(in) - Name of the user
     def setName(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Sets the contact of the user
+	# Side-effects: None
+	# User Contact(in) - Contact of the user
     def setContact(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+    # Postconditions: Sets the SSN of the user
+    # Side-effects: None
+    # User SSN(in) - SSN of the user
     def setSSN(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Sets the address of the user
+	# Side-effects: None
+	# User Address(in) - Address of the user
     def setAddress(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Sets the password of the user
+	# Side-effects: None
+	# User Password(in) - Password of the user
     def setPassword(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+	# Postconditions: Sets the Type of the user
+	# Side-effects: None
+	# User Type(in) - Type of the use
     def setType(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the id of the user
+	# Side-effects: None
+	# User ID(in) - Id of the user
     def getId(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the name of the user
+	# Side-effects: None
+	# User Name(out) - Name of the user
     def getName(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the contact of the user
+	# Side-effects: None
+	# User Contact(out) - Contact of the user
     def getContact(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the SSN of the user
+	# Side-effects: None
+	# User SSN(out) - SSN of the user
     def getSSN(self):
         pass
 
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the address of the user
+	# Side-effects: None
+	# User Address(out) - Address of the user
     def getAddress(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+    # Postconditions: Gets the password of the user
+    # Side-effects: None
+    # User Password(out) - Password of the user
     def getPassword(self):
         pass
 
+    # Preconditions: The user has to have been instantiated
+    # Postconditions: Gets the Type of the user
+    # Side-effects: None
+    # User Type(out) - Type of the user
     def getType(self):
         pass
