e3a5d79f92d4964da9e5684507fdde39ee94adf1
YazanSalem <yazansalem064@gmail.com>
2021-12-03 14:57:33 -0600

    File reorganizing and adding documentation

---
+++ TAScheduler/User/UserType.py
@@ -0,0 +1,15 @@
+class UserTyoe:
+
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Sets the Type of the user
+	# Side-effects: None
+	# User Type(in) - Type of the use
+    def setType():
+        pass
+
+	# Preconditions: The user has to have been instantiated
+	# Postconditions: Gets the Type of the user
+	# Side-effects: None
+	# User Type(out) - Type of the user
+    def getType():
+        pass
