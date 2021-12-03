b2a55c5b6d320668e68994017b5aecf474370f6b
YazanSalem <yazansalem064@gmail.com>
2021-12-03 15:53:33 -0600

    User and UserType completed

--- TAScheduler/User/UserType.py
+++ TAScheduler/User/UserType.py
@@ -1,15 +1,19 @@
-class UserTyoe:
+from abc import ABC, abstractmethod
+
+class UserType(ABC):
 
 	# Preconditions: The user has to have been instantiated
 	# Postconditions: Sets the Type of the user
 	# Side-effects: None
 	# User Type(in) - Type of the use
-    def setType():
-        pass
+	@abstractmethod
+	def setType(self, int):
+    		pass
 
 	# Preconditions: The user has to have been instantiated
 	# Postconditions: Gets the Type of the user
 	# Side-effects: None
 	# User Type(out) - Type of the user
-    def getType():
-        pass
+	@abstractmethod
+	def getType(self):
+		pass
