1ac2e399d57bf58f2a549988aa4abdd55261eabb
BMHebert <70716012+BMHebert@users.noreply.github.com>
2021-12-04 18:00:10 -0600

    Fixed an error
    Fixed an error that arose from the previous commit.

--- TAScheduler/models.py
+++ TAScheduler/models.py
@@ -24,8 +24,8 @@ class Lab(models.Model):
     location = models.CharField(max_length=20)
     hours = models.CharField(max_length=20)
     days = models.CharField(max_length=20)
-    instructor = models.ForeignKey(UserProfile.userID, on_delete=models.PROTECT)
-    TA = models.ForeignKey(UserProfile.userID, on_delete=models.PROTECT)
+    instructor = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+    TA = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="TAToLab")
 
 
 # The Course class keeps track of lecture sections and stores the TAs, instructors, and labs associated with it.
@@ -35,13 +35,13 @@ class Course(models.Model):
     location = models.CharField(max_length=20)
     hours = models.TimeField()
     days = models.CharField(max_length=20)
-    instructor = models.ForeignKey(UserProfile.userID, on_delete=models.PROTECT)
-    TAs = models.ManyToManyField(UserProfile.userID)
-    labs = models.ManyToManyField(Lab.labID)
+    instructor = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+    TAs = models.ManyToManyField(UserProfile, related_name="TAToCourse")
+    labs = models.ManyToManyField(Lab)
 
 
 # Schedule is a tool to be used by users to display the events they have going on in a week, be that Labs or courses.
 class Schedule(models.Model):
-    userID = models.ForeignKey(UserProfile.userID, on_delete=models.CASCADE)
-    courses = models.ManyToManyField(Course.courseID)
-    labs = models.ManyToManyField(Lab.labID)
+    userID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(Course)
+    labs = models.ManyToManyField(Lab)
