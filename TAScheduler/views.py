bfe795d51da6c767db9173a8f18ae369addc8463
cogii <kaihato@gmail.com>
2021-12-06 18:01:36 -0600

    added in possible necessary redirection methods in the Views.py class

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -6,3 +6,31 @@ from django.views import View
 class Login(View):
     def get(self, request):
         return render(request, "login.html", {})
+
+'''
+Skeleton for "possible" methods needed
+
+ --Redirect methods--
+
+ homepage_redir()
+ classSchedules_redir()
+ userMgmt_redir()
+ classLabManagement_redir()
+ accountSettings_redir()
+ courseTAAssignments_redir()
+ 
+ 
+ --Subpages--
+ 
+ userList_redir()
+ accCreation_redir()
+ classList_redir()
+ courseCreation_redir()
+ labList_redir()
+ courseAssignments_redir()
+ TAList_redir()
+ 
+ --errors--
+ 
+ 
+'''
