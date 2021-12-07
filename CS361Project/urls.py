0c6ee069d1551390b1038f48dcd2e2cd52db538a
cogii <kaihato@gmail.com>
2021-12-07 12:43:59 -0600

    instructor.py was an example from Apoorv on how we should create our classes to be used in the Views.py file, otherwise the Views.py file will get bogged down with too much code.
    urls.py was updated with pathing for redirects
    
    .html files are going to be worked on, with Sam focusing on getting the views.py files completed so we have a proper

--- CS361Project/urls.py
+++ CS361Project/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from TAScheduler.views import Login
+from django.views import View
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Login.as_view())
+    path('', Login.as_view()),
+    path('createuser', View.CreateUser),
+    path('createcourse', View.CreateCourse),
 ]
--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-
+from classses import Instructor,
 
 # Create your views here.
 class Login(View):
@@ -10,3 +10,11 @@ class Login(View):
 class Home(View):
     def get(self, request):
         return render(request, "home.html", {})
+
+class CreateUser(View):
+    def get(self, request):
+        return render(request, "createuser.html", {})
+
+class CreateCourse(View):
+    def get(self, request):
+        return render(request, "createcourse.html,", {})
