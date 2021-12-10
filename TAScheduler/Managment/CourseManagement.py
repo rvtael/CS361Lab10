f673ea19395002fbf731e61a5841cf200a44e695
bruggin9 <bruggin9@uwm.edu>
2021-12-10 11:15:24 -0600

    created editcourse.html and edituser.html . Views.py was the class I was looking at for the database stuff. The EditUser and EditCourse classes were the ones had the database information.

--- TAScheduler/Managment/CourseManagement.py
+++ TAScheduler/Managment/CourseManagement.py
@@ -6,56 +6,56 @@
 
 class CourseManagement:
 
-	# Preconditions: The user has to have been instantiated.
+    # Preconditions: The user has to have been instantiated.
     # The user must be of type administrator
     # Postconditions:Creates a course
-	# Side-effects: Course is created and added inside the database
-	# Course Name(in) - Name of the course
-	# Course Time(in) - Time of the course
-	# Course Days(in) - Days of the course
-	# Course Hoursin) - Hours of the course
-	# Course Instructor(in) - Instructor of the course
-	# Course TA(in) -TA of the course
+    # Side-effects: Course is created and added inside the database
+    # Course Name(in) - Name of the course
+    # Course Time(in) - Time of the course
+    # Course Days(in) - Days of the course
+    # Course Hoursin) - Hours of the course
+    # Course Instructor(in) - Instructor of the course
+    # Course TA(in) -TA of the course
     @staticmethod
     def createCourse(courseName, courseTime, courseDays, courseHours, courseInstructor, courseTA):
         pass
 
-	# Preconditions: The user has to have been instantiated.
+    # Preconditions: The user has to have been instantiated.
     # The user must be of type administrator
     # Postconditions:Edits a course
-	# Side-effects: Course is edited inside the database
-	# Course Name(in) - Name of the course
-	# Course Time(in) - Time of the course
-	# Course Days(in) - Days of the course
-	# Course Hoursin) - Hours of the course
-	# Course Instructor(in) - Instructor of the course
-	# Course TA(in) -TA of the course
+    # Side-effects: Course is edited inside the database
+    # Course Name(in) - Name of the course
+    # Course Time(in) - Time of the course
+    # Course Days(in) - Days of the course
+    # Course Hoursin) - Hours of the course
+    # Course Instructor(in) - Instructor of the course
+    # Course TA(in) -TA of the course
     @staticmethod
     def editCourse(course, courseName, courseTime, courseDays, courseHours, courseInstructor, courseTA):
         pass
 
-	# Preconditions: The user has to have been instantiated.
+    # Preconditions: The user has to have been instantiated.
     # The user must be of type administrator
     # Postconditions:Deletes a course
-	# Side-effects: Course is deleted and removed from the database
-	# Course Name(in) - Name of the course
+    # Side-effects: Course is deleted and removed from the database
+    # Course Name(in) - Name of the course
     @staticmethod
     def deleteCourse(course, courseName):
         pass
 
-	# Preconditions: The user has to have been instantiated
-	# The searchPrompt is an existing course assignment name
-	# Postconditions: Course assignments are populated
-	# Side-effects: None
-	# Search Prompt(in): Course Name you are searching for
+    # Preconditions: The user has to have been instantiated
+    # The searchPrompt is an existing course assignment name
+    # Postconditions: Course assignments are populated
+    # Side-effects: None
+    # Search Prompt(in): Course Name you are searching for
     @staticmethod
     def populateSearchClass(searchPromp):
         pass
 
-	# Preconditions: The user has to have been instantiated
-	# There are courses to display
-	# Postconditions: All courses are displayed
-	# Side-effects: None
+    # Preconditions: The user has to have been instantiated
+    # There are courses to display
+    # Postconditions: All courses are displayed
+    # Side-effects: None
     @staticmethod
     def displayAllCourse():
         pass
