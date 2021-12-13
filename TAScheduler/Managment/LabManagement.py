c290155e9051d0a45134b685ed81eb0a592dea78
YazanSalem <yazansalem064@gmail.com>
2021-12-12 22:12:03 -0600

    Lab Management issues fixed

--- TAScheduler/Managment/LabManagement.py
+++ TAScheduler/Managment/LabManagement.py
@@ -1,6 +1,7 @@
+
 from django.db import models
 from TAScheduler.models import Lab
-from TAScheduler.User.User import User
+from TAScheduler.models import UserProfile
 from django.shortcuts import render
 
 class LabManagement(object):
@@ -15,23 +16,8 @@ class LabManagement(object):
 #    days = models.CharField(max_length=20)
 #    instructor = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
 #    TA = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="TAToLab")
-    def __init__(self, authID=""):
-        pass
-
 	# Preconditions: The user has to have been instantiated.
-    # The user must be of type administrator
-    # Postconditions:Creates a lab
-<<<<<<< Updated upstream
-	# Side-effects: Lab is created and added inside the database
-	# Lab Name(in) - Name of the lab
-	# Lab Hours(in) - Hours of the lab
-	# Lab Location(in) - Location of the lab
-	# Lab Days(in) - Days of the lab
-	# Lab Instructor(in) - Instructor of the lab
-	# Lab TA(in) -TA of the lab
-    def createLab(self, labName, labHours, labLocation, labDays, labTA):
-        pass
-=======
+
     # Side-effects: Lab is created and added inside the database
     #Lab ID(in) - Id of the lab
     # Lab Name(in) - Name of the lab
@@ -51,35 +37,21 @@ class LabManagement(object):
             raise TypeError("Lab Hours entered is not of type str")
         if(not(isinstance(labDays, str))):
             raise TypeError("Lab Days entered is not of type str")
-        if(not(isinstance(labInstructor, User))):
+        if(not(isinstance(labInstructor, UserProfile))):
             raise TypeError("Lab Instructor entered is not of type User")
-        if(labInstructor.getType() != "INSTRUCTOR"):
+        if(labInstructor.userType != "INSTRUCTOR"):
             raise TypeError("Lab Instructor's type is not of type INSTRUCTOR")
-        if (not(isinstance(labTA, User))):
+        if (not(isinstance(labTA, UserProfile))):
             raise TypeError("Lab TA entered is not of type User")
-        if(labTA.getType() != "TA"):
+        if(labTA.userType != "TA"):
             raise TypeError("Lab TA's type is not of type TA")
 
-        newLab = Lab.objects.create()
+        newLab = Lab.objects.create(labID=Id, name=labName, location=labLocation, 
+        hours=labHours, days=labDays, instructor=labInstructor, TA=labTA)
         newLab.save()
 
-        return render(request, "placeholder",{})
->>>>>>> Stashed changes
+        return "Lab was created"
 
-	# Preconditions: The user has to have been instantiated.
-    # The user must be of type administrator
-    # Postconditions:Edits a lab
-<<<<<<< Updated upstream
-	# Side-effects: Lab is edited inside the database
-	# Lab Name(in) - Name of the lab
-	# Lab Hours(in) - Hours of the lab
-	# Lab Location(in) - Location of the lab
-	# Lab Days(in) - Days of the lab
-	# Lab Instructor(in) - Instructor of the lab
-	# Lab TA(in) -TA of the lab
-    def editLab(self, labName, labHours, labLocation, labDays, labTA):
-        pass
-=======
     # Side-effects: Lab is edited inside the database
     #Lab ID(in) - Id of the lab
     # Lab Name(in) - Name of the lab
@@ -99,19 +71,19 @@ class LabManagement(object):
             raise TypeError("Lab Hours entered is not of type str")
         if(not(isinstance(labDays, str))):
             raise TypeError("Lab Days entered is not of type str")
-        if(not(isinstance(labInstructor, User))):
+        if(not(isinstance(labInstructor, UserProfile))):
             raise TypeError("Lab Instructor entered is not of type User")
-        if(labInstructor.getType() != "INSTRUCTOR"):
+        if(labInstructor.userType != "INSTRUCTOR"):
             raise TypeError("Lab Instructor's type is not of type INSTRUCTOR")
-        if (not(isinstance(labTA, User))):
+        if (not(isinstance(labTA, UserProfile))):
             raise TypeError("Lab TA entered is not of type User")
-        if(labTA.getType() != "TA"):
+        if(labTA.userType != "TA"):
             raise TypeError("Lab TA's type is not of type TA")
         
-        if(not(Lab.objects.filter(LabId = Id).exists())):
+        if(not(Lab.objects.filter(labId = Id).exists())):
             raise TypeError("This Lab does not exist")
 
-        edittedLab = Lab.objects.get(LabId = Id)
+        edittedLab = Lab.objects.get(labId = Id)
         
         edittedLab.name = labName
         edittedLab.location = labLocation
@@ -120,46 +92,24 @@ class LabManagement(object):
         edittedLab.instructor = labInstructor
         edittedLab.TA = labTA
 
-        return render(request, "placeholder","The lab was successfully editted")
->>>>>>> Stashed changes
+        return "The lab was successfully editted"
 
 	# Preconditions: The user has to have been instantiated.
     # The user must be of type administrator
     # Postconditions:Deletes a lab
-<<<<<<< Updated upstream
 	# Side-effects: Lab is deleted and removed from the database
 	# Lab Name(in) - Name of the course
-    def deleteLab(self, labName):
-        pass
-
-	# Preconditions: The user has to have been instantiated
-	# The searchPrompt is an existing lab assignment name
-	# Postconditions: Lab assignments are populated
-	# Side-effects: None
-	# Search Prompt(in): Lab Name you are searching for
-    def populateSearchLab(self, searchPrompt):
-        pass
-
-	# Preconditions: The user has to have been instantiated
-	# There are labs to display
-	# Postconditions: All labs are displayed
-	# Side-effects: None
-    def displayAllLabs(self):
-        pass
-=======
-    # Side-effects: Lab is deleted and removed from the database
-    # Lab ID(in) - ID of the Lab
     def deleteLab(request, Id):
         if(not(isinstance(Id, int))):
             raise TypeError("Id entered is not of type int")
 
         retMsg = "Lab has been successfully deleted"
-        if(not(Lab.objects.filter(LabId = Id).exists())):
+        if(not(Lab.objects.filter(labId = Id).exists())):
             retMsg = "This lab being deleted does not exist"
         else:
-            Lab.objects.filter(LabId = Id).delete()
+            Lab.objects.filter(labId = Id).delete()
 
-        return render(request, "Placeholder", retMsg)
+        return retMsg
         
 
     # Preconditions: The user has to have been instantiated
@@ -172,14 +122,14 @@ class LabManagement(object):
             raise TypeError("Id entered is not of type int")
         
         retMsg = ""
-        if(not(Lab.objects.filter(LabId = Id).exists())):
+        if(not(Lab.objects.filter(labId = Id).exists())):
             retMsg = "This lab being deleted does not exist"
         else:
             retMsg = {
-                'Found Lab': Lab.objects.get(LabId = Id)
+                'Found Lab': Lab.objects.get(labId = Id)
             }
 
-        return render(request,"Placeholder", retMsg)
+        return retMsg
         
 
     # Preconditions: The user has to have been instantiated
@@ -193,5 +143,4 @@ class LabManagement(object):
             'All Labs': allLabs
         }
         
-        return render(request, "Placeholder", data)
->>>>>>> Stashed changes
+        return allLabs
